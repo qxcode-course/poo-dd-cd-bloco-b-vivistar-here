9758dee3ddefee18ec3075ea79688394af00d751
Vitoria de Jesus <vitoriadejesusstar@alu.ufc.br>
2025-10-11 23:59:55 +0000

    estruturas

---
+++ myrep/poo/relogio/py/draft.py
@@ -0,0 +1,19 @@
+class Watch:
+    def __init__(self):
+        self.hour = 0
+        self.minute = 0
+        self.second = 0
+
+    def getHour(self):
+
+    def getMinute(self):
+
+    def getSecond(self):
+
+    def setHour():
+
+    def setMinute():
+
+    def setSecond():
+
+    def nextSecond():
