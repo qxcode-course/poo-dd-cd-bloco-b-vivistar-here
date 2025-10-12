998b343c52db2e635e30319ac1ac96d551a0d4c8
Vitoria de Jesus <vitoriadejesusstar@alu.ufc.br>
2025-10-12 00:59:31 +0000

    hora da ceia :P

--- myrep/poo/relogio/py/draft.py
+++ myrep/poo/relogio/py/draft.py
@@ -14,7 +14,7 @@ class Watch:
         return self.second
 
     def setHour(self, hour):
-        if 0 >= hour <= 23:
+        if 0 <= hour <= 23:
             self.hour = hour
 
     def setMinute(self, minute):
@@ -27,21 +27,21 @@ class Watch:
 
     def nextSecond(self):
         self.second += 1
-        if self.second > 59:
-            self.second = 0
+    if self.second > 59:
+        self.second = 0
         self.minute += 1
         if self.minute > 59:
             self.minute = 0
-        self.hour += 1
-        if self.hour > 23:
-            self.hour = 0
-
+            self.hour += 1
+            if self.hour > 23:
+                self.hour = 0
+                
     def __str__(self):
         return f"{self.hour:02d}:{self.minute:02d}:{self.second:02d}"
 
 
 def main():
-    watch = Watch(0)
+    watch = Watch()
     while True:
         line: str = input()
         print("$" + line)
@@ -50,7 +50,28 @@ def main():
         if args[0] == "end":
             break
         elif args[0] == "init":
+            h = int(args[1])
+            m = int(args[2])
+            s = int(args[3])
+            watch.setHour(h)
+            watch.setMinute(m)
+            watch.setSecond(s)
+        
+        elif args[0] == "set":
+            if args[1] == "hour":
+                 watch.setHour(int(args[2]))
+            elif args[1] == "minute":
+                watch.setMinute(int(args[2]))
+            elif args[1] == "second":
+                watch.setSecond(int(args[2]))
 
+        elif args[0] == "show":
+            print(watch)
 
+        elif args[0] == "next":
+            watch.nextSecond()
 
+        else:
+            print("fail: comando invalido")
+        
 main() 
