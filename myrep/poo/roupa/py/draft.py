4c33ea16d8af49e1d42d75b2ee69f9800fa5f502
Vitoria de Jesus <vitoriadejesusstar@alu.ufc.br>
2025-10-13 03:53:10 +0000

    roupa

--- myrep/poo/roupa/py/draft.py
+++ myrep/poo/roupa/py/draft.py
@@ -1,2 +1,39 @@
 class Roupa:
-    def __init__(self)
+    def __init__(self):
+        self.__tamanho: str = ""
+
+    def getTamanho(self) -> str: 
+        return self.__tamanho
+
+    def setTamanho(self, value: str):
+        tamanhosValidos = ["PP", "P", "M", "G", "GG", "XG"]
+        if value in tamanhosValidos:
+            self.__tamanho = value
+        else:
+            print("fail: Valor inválido, tente PP, P, M, G, GG ou XG")
+
+    def __str__(self):
+        return f"size: ({self.__tamanho})"
+
+def main():
+    roupa = Roupa()
+    while True: 
+        line: str = input()
+        print("$" + line)
+        args: list[str] = line.split()
+
+        if len(args) == 0:
+            continue
+        if args[0] == "end":
+            break
+        elif args[0] == "show":
+            print(roupa)
+        elif args[0] == "size":
+            if len(args) > 1:
+                roupa.setTamanho(args[1])
+            else:
+                print("fail: informe o tamanho desejado")
+        else: 
+            print("fail: comando desconhecido")
+
+main()
