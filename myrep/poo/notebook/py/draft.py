14af9daec26c0dc82df45e1e64ac21930440f1bb
Vitoria de Jesus <vitoriadejesusstar@alu.ufc.br>
2025-10-20 19:17:50 +0000

    ligar e desligar

--- myrep/poo/notebook/py/draft.py
+++ myrep/poo/notebook/py/draft.py
@@ -1,8 +1,23 @@
 class Notebook:
     def __init__(self):
         self.__ligado: bool = False
+        self.mostrar(mostrar)
+        self.usar(usar)
+        self.ligar(ligar)
+
+    def ligar(self):
+        if self.__ligado:
+            self.__ligado = True
+            print("Notebook ligando")
+        else:
+            print("O notebook já está ligado")
         
-        
+    def desligar(self):
+        if self.__ligado:
+            self.ligado = Falso
+            print("Notebook desligando")
+        else:
+            print("Notebook já está desligado")
         
         notebook = Notebook()
 notebook.mostrar()
@@ -10,6 +25,16 @@ notebook.usar(10)
 notebook.ligar()
 notebook.mostrar()
 notebook.usar(10)
-notebook.ligar()
+notebook.desligar()
 
+def main():
+    notebook = Notebook()
+    while True:
+        line = input()
+        args = line.split(" ")
+        print(f"&{line}")
 
+    match args[0] == "mostrar":
+    print(Notebook)
+
+main()
