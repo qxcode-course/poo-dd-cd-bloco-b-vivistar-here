e7fb77bde8488a1a1b04f629c1f57fa9347dd43d
Vitoria de Jesus <vitoriadejesusstar@alu.ufc.br>
2025-10-20 18:57:22 +0000

    parte 1 - esqueleto

---
+++ myrep/poo/notebook/py/draft.py
@@ -0,0 +1,15 @@
+class Notebook:
+    def __init__(self):
+        self.__ligado: bool = False
+        
+        
+        
+        notebook = Notebook()
+notebook.mostrar()
+notebook.usar(10)
+notebook.ligar()
+notebook.mostrar()
+notebook.usar(10)
+notebook.ligar()
+
+
