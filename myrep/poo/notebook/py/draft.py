56fe21ae500087e2cd60f99718b3569b39c8d4f9
Vitoria de Jesus <vitoriadejesusstar@alu.ufc.br>
2025-10-25 19:18:41 +0000

    bateria

--- myrep/poo/notebook/py/draft.py
+++ myrep/poo/notebook/py/draft.py
@@ -1,41 +1,63 @@
 class Bateria:
     def __init__(self, capacidade: int):
         self.__capacidade: int = capacidade
-        self.__carga: int = capacidade
+        self.__bateria: int = capacidade
 
     def getCapacidade(self) -> int:
         return self.__capacidade
 
-    def getCarga(self) -> int:
-        return self.__carga
+    def getBateria(self) -> int:
+        return self.__bateria
 
-    def setCarga(self, value: int):
+    def setBateria(self, value: int):
         if value < 0:
-            self.__carga = 0
+            self.__bateria = 0
         elif value > self.__capacidade:
-            self.__carga = self.__capacidade
+            self.__bateria = self.__capacidade
         else:
-            self.__carga = value
+            self.__bateria = value
 
-    def descarregr(self, tempo: int) -> int:
-        if tempo <= self.__carga:
-            self.__carga -= tempo
+    def descarregar(self, tempo: int) -> int:
+        if tempo <= self.__bateria:
+            self.__bateria -= tempo
             return tempo
         else:
-            usado = self.__carga 
-            self.__carga = 0
+            usado = self.__bateria 
+            self.__bateria = 0
             return usado
 
     def mostrar(self):
-        print(f"({self.__carga}/{self.__capacidade})")
+        print(f"({self.__bateria}/{self.__capacidade})")
 
     def __str__(self):
-        return f"({self.__carga}/{self.__capacidade})"
+        return f"({self.__bateria}/{self.__capacidade})"
 
 
 class Notebook:
     def __init__(self):
         self.__ligado: bool = False
+        self.__bateria: Bateria | None = None
+
+    def getBateria(self) -> Bateria | None:
+        return self.__bateria
+
+    def setBateria(self, bateria: Bateria):
+        if self.__bateria is not None:
+            print("fail: já existe uma bateria instalada")
+        else:
+            self.__bateria = bateria
+            print("bateria inserida")
+    
+    def rmBateria(self):
+        if self.__bateria is None:
+            print("fail: nenhuma bateria para remover")
+            return None
+        else:
+            print("bateria removida")
+            bateria = self.__bateria
+            self.__bateria = None
+            self.__ligado = False
+            return bateria
       
     def ligar(self):
         if not self.__ligado:
@@ -46,7 +68,7 @@ class Notebook:
         
     def desligar(self):
         if not self.__ligado:
-            self.ligado = Falso
+            self.ligado = False
             print("Notebook desligando")
         else:
             print("Notebook já está desligado")
@@ -74,9 +96,7 @@ def main():
         args = line.split(" ")
         print(f"&{line}")
 
-        if len(args) == 0:
-            continue
-
+    
         if args[0] == "end":
             break
         elif args[0] == "show":
@@ -90,31 +110,24 @@ def main():
                 notebook.usar(int(args[1]))
             else:
                 print("Fail: informe o tempo de uso em horas")
-        else:
-            print("Fail: comando inválido")
-
         elif args[0] == "bateria":
             if len(args) > 1 and args[1].isdigit():
                 bateria = Bateria(int(args[1]))
                 print(f"Bateria criada com capacidade {args[1]}")
             else:
                 print("fail: informe a capacidadde da bateria")
-
         elif args[0] == "mostrarbateria":
             if bateria is not None:
                 bateria.mostrar()
             else:
                 print("fail: nenhuma bateria foi criada")
-            
         elif args[0] == "setbateria":
             if baterua is not None:
                 notebook.setBateria(bateria)
             else:
                 print("fail: nenhuma bateria disponível")
-
-        elif args[0] == "descarregar":
-            bateria = notebook.descarregar()
-
+        elif args[0] == "rmbateria":
+            bateria = notebook.rmBateria()
         else:
             print("fail: comando inválido")
 main()
