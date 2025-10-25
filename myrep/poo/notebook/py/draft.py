7e0b15e4fe652d56244de18f0660d4ca1fb25788
Vitoria de Jesus <vitoriadejesusstar@alu.ufc.br>
2025-10-25 19:46:35 +0000

    carregador

--- myrep/poo/notebook/py/draft.py
+++ myrep/poo/notebook/py/draft.py
@@ -1,42 +1,60 @@
 class Bateria:
     def __init__(self, capacidade: int):
         self.__capacidade: int = capacidade
-        self.__bateria: int = capacidade
+        self.__carga: int = capacidade
 
     def getCapacidade(self) -> int:
         return self.__capacidade
 
-    def getBateria(self) -> int:
-        return self.__bateria
+    def getCarga(self) -> int:
+        return self.__carga
 
-    def setBateria(self, value: int):
+    def setCarga(self, value: int):
         if value < 0:
-            self.__bateria = 0
+            self.__carga = 0
         elif value > self.__capacidade:
-            self.__bateria = self.__capacidade
+            self.__carga = self.__capacidade
         else:
-            self.__bateria = value
+            self.__carga = value
 
     def descarregar(self, tempo: int) -> int:
-        if tempo <= self.__bateria:
-            self.__bateria -= tempo
+        if tempo <= self.__carga:
+            self.__carga -= tempo
             return tempo
         else:
             usado = self.__bateria 
-            self.__bateria = 0
+            self.__carga = 0
             return usado
 
+    def carregar(self, tempo:  int, potencia: int):
+        nova_carga = self.__carga + tempo * potencia
+        self.setCarga(nova_carga)
+
     def mostrar(self):
-        print(f"({self.__bateria}/{self.__capacidade})")
+        print(f"({self.__carga}/{self.__capacidade})")
 
     def __str__(self):
-        return f"({self.__bateria}/{self.__capacidade})"
+        return f"({self.__carga}/{self.__capacidade})"
+
+class Carregador:
+    def __init__(self, potencia: int):
+        self.__potencia: int = potencia
+    
+    def getPotencia(self)-> int:
+        return self.__potencia
+
+    def mostrar(self):
+        print(f"(Potência {self.__potencia})")
+
+    def __str__(self):
+        return f"(Potência {self.__potencia})"
 
 
 class Notebook:
     def __init__(self):
         self.__ligado: bool = False
         self.__bateria: Bateria | None = None
+        self.__carregador: Carregador | None = None
 
     def getBateria(self) -> Bateria | None:
         return self.__bateria
@@ -58,16 +76,36 @@ class Notebook:
             self.__bateria = None
             self.__ligado = False
             return bateria
+
+    def setCarregador(self, carregador: Carregador):
+        if self.__carregador is not None:
+            print("fail: ja existe um carregador conectado")
+        else:
+            self.__carregador = carregador
+            print("carregador conectado")
+
+    def rmCarregador(self):
+        if self.__carregador is None:
+            print("fail: nenhum carregador para remover")
+        else:
+            print("carregador removido")
+            self.__carregador = None
       
     def ligar(self):
-        if not self.__ligado:
+        if self.__ligado:
+            print("notebook já está ligado")
+            return
+        if self.__bateria and self.__bateria.getCarga() > 0:
             self.__ligado = True
-            print("Notebook ligando")
+            print("notebook ligado")
+        elif self.__carregador is not None:
+            self.__ligado = True
+            print("notebook ligado (usando carregador)")
         else:
-            print("O notebook já está ligado")
-        
+            print("fail: nao foi possível ligar (sem bateria nem carregador)")
+
     def desligar(self):
-        if not self.__ligado:
+        if self.__ligado:
             self.ligado = False
             print("Notebook desligando")
         else:
