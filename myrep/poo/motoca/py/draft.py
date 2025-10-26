e2a8de6ebc01931f13616ca55633b5568c3b59e3
Vitoria de Jesus <vitoriadejesusstar@alu.ufc.br>
2025-10-26 00:13:45 +0000

    ncmajskaçaaaaaaaaaaaaa

--- myrep/poo/motoca/py/draft.py
+++ myrep/poo/motoca/py/draft.py
@@ -26,12 +26,87 @@ class Motoca:
         self.__pessoa = pessoa
         return True
 
-    def remover
+    def remover(self) -> Pessoa | None:
+        if self.__pessoa is None:
+            print("fail: empty motorcycle")
+            return None
+        pessoa = self.__pessoa
+        self.__pessoa = None
+        return pessoa
 
-    def buyTime
+    def buyTime(self, time: int):
+        self.__time += time
 
-    def drive
+    def drive(self, time: int):
+        if self.__time <= 0:
+            print("fail: buy time first")
+            return
+        if self.__pessoa is None:
+            print("fail: empty motorcycle")
+            return
+        if self.__pessoa.getAge() > 10:
+            print("fail: too old to drive")
+            return
+        if time > self.__time:
+            print("fail: time finished after {self.__time} minutes")
+            self.__time = 0
+        else:
+            self.__time -= time
 
-    def honk
+    def honk(self):
+        print("P" + "e" * self.__potencia + "m!")
 
-    def __str__
+    def __str__(self):
+        pessoa_str = "empty" if self.__pessoa is None else str(self.__pessoa)
+        return f"potencia: {self.__potencia}, time: {self.__time}, pessoa: ({pessoa_str})"
+
+def main():
+    moto = Motoca()
+    pessoa: Pessoa | None = None
+
+    while True:
+        line = input()
+        args = line.split()
+        print("${line}")
+
+        if args[0] == "end":
+            break
+
+        elif args[0] == "init":
+            moto = Motoca()
+
+        elif args[0] == "show":
+            print(moto)
+
+        elif args[0] == "enter":
+            name = args[1]
+            age = int(args[2])
+            pessoa = Pessoa(name, age)
+            moto.inserir(pessoa)
+            else: 
+                print("fail: enter name age")
+        
+        elif args[0] == "leave":
+            pessoa = moto.remover()
+            if pessoa:
+                print(f"{pessoa} saiu da motoca")
+
+        elif args[0] == "buy":
+            if len(args) == 2 and args[1].isdigit():
+                moto.buyTime(int(args[1]))
+            else:
+                print("fail: informe tempo em minutos")
+        
+        elif args[0] == "drive":
+            if len(args) == 2 and args[1].isdigit():
+                moto.drive(int(args[1]))
+            else:
+                print("fail: informe o tempo de direção")
+
+        elif args[0] == "honk":
+            moto.honk()
+
+        else:
+            print("fail: comando invalido")      
+
+main()
