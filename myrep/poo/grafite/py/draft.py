97091befe5d0d6ccdfc9912359f63afe1d1e4fa2
Vitoria de Jesus <vitoriadejesusstar@alu.ufc.br>
2025-10-26 02:14:07 +0000

    mds vou dormir ou desmaio

--- myrep/poo/grafite/py/draft.py
+++ myrep/poo/grafite/py/draft.py
@@ -18,7 +18,7 @@ class Grafite:
 
     def usagePerSheet(self) -> int:
         gasto = {"HB": 1, "2B": 2, "4B": 4, "6B": 6}
-        return gasto.get(getDureza(), 0)
+        return gasto.get(self.getDureza(), 0)
 
     def __str__(self):
         return f"{self.__calibre:.1f}:{self.__dureza}:{self.__tamanho}"
@@ -33,16 +33,16 @@ class Lapiseira:
     
     def insert(self, grafite: Grafite):
         if self.hasGrafite():
-            print("Já existe grafite na lapiseira")
+            print("fail: ja existe grafite")
             return
         if grafite._Grafite__calibre != self.__calibre:
-            print("Calibre incompatível")
+            print("fail: calibre incompativel")
             return
-            self.__grafite = grafite
+        self.__grafite = grafite
 
     def remove(self) -> Grafite | None:
         if not self.hasGrafite():
-            print("fail: não existe grafite na lapiseira")
+            print("fail: nao existe grafite")
             return None
         grafite = self.__grafite 
         self.__grafite = None
@@ -50,11 +50,17 @@ class Lapiseira:
 
     def write(self):
         if not self.hasGrafite():
-            print("fail: não existe grafite na lapiseira")
+            print("fail: nao existe grafite")
             return
+
+        grafite = self.__grafite
+        tamanho = grafite.getTamanho()
+        gasto = grafite.usagePerSheet()
+
         if tamanho <= 10:
-            print("fail: tamanho insuficiente de grafite")
+            print("fail: tamanho insuficiente")
             return 
+
         if tamanho - gasto < 10:
             grafite.setTamanho(10)
             print("fail: folha incompleta")
@@ -65,10 +71,50 @@ class Lapiseira:
 
     def __str__(self):
         if self.__grafite:
-            return f"calibre:{self.__calibre:.1f}, grafite:{self.__grafite}"
+            return f"calibre: {self.__calibre:.1f}, grafite: [{self.__grafite}]"
         else:
-            return f"calibre:{self.__calibre:.1f}, grafite:(null)"
+            return f"calibre: {self.__calibre:.1f}, grafite: null"
 
 def main():
+    lapiseira: Lapiseira | None = None
+    grafite: Grafite | None = None
+
+    while True:
+        line = input()
+        args = line.split()
+        print("$" + line)
+
+        if args[0] == "end":
+            break
+
+        elif args[0] == "init":
+            lapiseira = Lapiseira(float(args[1]))
+
+        elif args[0] == "insert":
+            if lapiseira is None:
+                print("fail: lapiseira não inicializada")
+                continue
+            grafite = Grafite (float(args[1]), args[2], int(args[3]))
+            lapiseira.insert(grafite)
+
+        elif args[0] == "remove":
+            if lapiseira is None:
+                print("fail: lapiseira não inicializada")
+                continue
+            grafite = lapiseira.remove()
+
+        elif args[0] == "write":
+            if lapiseira is None:
+                print("fail: lapiseira não inicializada")
+                continue
+            lapiseira.write()
+
+        elif args[0] == "show":
+            if lapiseira:
+                print(lapiseira)
+            else:
+                print("fail: lapiseira não inicializada")
+        else:
+            print("fail: comando inválido")
 
 main()
